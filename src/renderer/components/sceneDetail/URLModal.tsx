f7cdc6a7f72b22e197ff10ecebafae9dadcdaea2
ififfy <ififfy@mm.st>
2018-12-27 23:29:26 -0800

    Split file

--- src/renderer/components/sceneDetail/URLModal.tsx
+++ src/renderer/components/sceneDetail/URLModal.tsx
@@ -1,43 +1,8 @@
 import * as React from 'react'
 import Modal from "../ui/Modal";
 import GooninatorImporter from './GooninatorImporter';
+import ImageURLListImporter from './ImageURLListImporter';
 import { TabbedSections } from '../ui/TabbedSections';
-import SimpleURLInput from '../ui/SimpleURLInput';
-
-class ImageURLListImporter extends React.Component {
-  readonly props: {
-    directories: Array<string>,
-    onDidImport(): void,
-    onChangeDirectories(directories: Array<string>): void,
-    onChangeTextKind(kind: string) : void,
-    onChangeTextSource(hbID: string) : void,
-  };
-
-  state = {
-    url: "",
-  }
-
-  render() {
-    return (
-      <div className="ImageURLListImporter">
-        <SimpleURLInput
-          label="URL to a raw text file containing image URLs on separate lines"
-          isEnabled={true}
-          value={this.state.url}
-          onChange={(value) => this.setState({url: value})} />
-        <br />
-        <div style={{clear: 'both'}} />
-        <div className="u-button u-float-right" onClick={this.import.bind(this)}>OK</div>
-        <div style={{clear: 'both'}} />
-      </div>
-    );
-  }
-
-  import() {
-    this.props.onChangeDirectories(this.props.directories.concat([this.state.url]))
-    this.props.onDidImport();
-  }
-}
 
 export default class URLModal extends React.Component {
   readonly props: {
