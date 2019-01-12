511b4c0adf3d80a510129f957651cf87d83cc0e6
GoddessServant0 <goddessservant0@gmail.com>
2019-01-12 09:22:44 -0500

    Decode path in win32 systems

--- src/renderer/urlToPath.ts
+++ src/renderer/urlToPath.ts
@@ -3,7 +3,7 @@ import { URL } from "url";
 export default function urlToPath(url: string): string {
     const path = new URL(url).pathname;
     if (process.platform === "win32") {
-      return path.substring(1, path.length);
+      return decodeURIComponent(path.substring(1, path.length));
     } else {
       return path;
     }
