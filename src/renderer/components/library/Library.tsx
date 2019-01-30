928a92a5444f9d0211b86b41f74ffb399038c7dd
GoddessServant0 <goddessservant0@gmail.com>
2019-01-29 19:31:43 -0500

    Hide import (ctrl+P) and add URLs with simple input. Add edit URL

--- src/renderer/components/library/Library.tsx
+++ src/renderer/components/library/Library.tsx
@@ -15,11 +15,12 @@ export default class Library extends React.Component {
   readonly state = {
     removeAllIsOpen: false,
     urlImportIsOpen: false,
+    isEditing: String(null),
   };
 
   render() {
     return (
-      <div className="Library">
+      <div className="Library" onKeyDown={this.secretHotkey.bind(this)} tabIndex={0}>
         <div className="u-button-row">
           <div className="u-abs-center">
               <h2 className="Library__LibraryHeader">Library</h2>
@@ -30,7 +31,7 @@ export default class Library extends React.Component {
 
         <div className="Library__Buttons">
           <div className='u-button u-clickable' onClick={this.onAdd.bind(this)}>+ Add local files</div>
-          <div className='u-button u-clickable' onClick={this.toggleURLImportModal.bind(this)}>+ Import URL</div>
+          <div className='u-button u-clickable' onClick={this.onAddURL.bind(this)}>+ Add URL</div>
           <div className={`u-button u-float-left ${this.props.library.length == 0 ? 'u-disabled' : 'u-clickable'} `}
                onClick={this.props.library.length == 0 ? this.nop : this.toggleRemoveAllModal.bind(this)}>- Remove All</div>
         </div>
@@ -44,11 +45,26 @@ export default class Library extends React.Component {
           {this.props.library.map((source) =>
             <div className="Library__Source"
                  key={(source.url as any) as number}>
-              <div className="Library__SourceTitle" onClick={this.props.onPlay.bind(this, source.url)}>
-                {source.url}
-              </div>
+              {this.state.isEditing != source.url && (
+                <div className="Library__SourceTitle u-clickable" onClick={this.props.onPlay.bind(this, source.url)}>
+                  {source.url}
+                </div>
+              )}
+              {this.state.isEditing == source.url && (
+                <form className="Library__SourceTitle" onSubmit={this.onEdit.bind(this, null)}>
+                  <input
+                      autoFocus
+                      type="text"
+                      value={source.url}
+                      onBlur={this.onEdit.bind(this, null)}
+                      onChange={this.onEditSource.bind(this, source.url)} />
+                </form>
+              )}
+
               <div className="u-button u-destructive u-clickable"
-                   onClick={this.onRemove.bind(this, source.url)}>×️</div>
+                 onClick={this.onRemove.bind(this, source.url)}>×️</div>
+              <div className="u-button u-edit u-clickable"
+                 onClick={this.onEdit.bind(this, source.url)}/>
             </div>
           )}
         </div>
@@ -90,6 +106,44 @@ export default class Library extends React.Component {
     this.setState({urlImportIsOpen: !this.state.urlImportIsOpen});
   }
 
+  // Use alt+P to access import modal
+  secretHotkey(e: KeyboardEvent) {
+    if (e.altKey && e.key=='p') {
+      this.toggleURLImportModal();
+    }
+  }
+
+  onEdit(source: string, e: Event) {
+    e.preventDefault();
+    // If user left input blank, remove it from list of sources
+    let newLibrary = [];
+    let needsUpdate = false;
+    for (let source of this.props.library) {
+      if (source.url == "") {
+        needsUpdate = true;
+      } else {
+        newLibrary.push(source);
+      }
+    }
+    this.setState({isEditing: source});
+    if (needsUpdate) { // Only update sources if we need to
+      this.props.onUpdateLibrary(newLibrary);
+    }
+  }
+
+  onEditSource(oldURL: string, e: React.FormEvent<HTMLInputElement>) {
+    // Use the oldURL to make sure we edit the right source
+    let newLibrary = this.props.library;
+    for (let source of newLibrary) {
+      if (source.url == oldURL) {
+        source.url = e.currentTarget.value;
+        break;
+      }
+    }
+    this.setState({isEditing: e.currentTarget.value});
+    this.props.onUpdateLibrary(newLibrary);
+  }
+
   removeAll() {
     this.toggleRemoveAllModal();
     this.props.onUpdateLibrary([]);
@@ -103,6 +157,11 @@ export default class Library extends React.Component {
     let result = remote.dialog.showOpenDialog({properties: ['openDirectory', 'multiSelections']});
     if (!result) return;
     this.addDirectories(result);
+  }
+
+  onAddURL() {
+    this.setState({isEditing: ""});
+    this.addDirectories([""]);
   }
 
   addDirectories(directories: Array<string>) {
