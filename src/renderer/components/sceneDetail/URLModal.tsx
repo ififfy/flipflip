9270db09fbaff0582fa687f5b551ce81052b8743
GoddessServant0 <goddessservant0@gmail.com>
2019-01-30 21:54:21 -0500

    Remove ImageURLListImporter

--- src/renderer/components/sceneDetail/URLModal.tsx
+++ src/renderer/components/sceneDetail/URLModal.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react'
 import Modal from "../ui/Modal";
 import GooninatorImporter from './GooninatorImporter';
-import ImageURLListImporter from './ImageURLListImporter';
-import { TabbedSections } from '../ui/TabbedSections';
 
 export default class URLModal extends React.Component {
   readonly props: {
@@ -15,21 +13,11 @@ export default class URLModal extends React.Component {
   render() {
     return (
       <Modal onClose={this.props.onClose} title="Import URL">
-        <TabbedSections
-          style={{maxWidth: '100%', width: '300px', minHeight: '220px'}}
-          titles={['Image URL list', 'Gooninator']}
-          renderChildren={() => [
-              <ImageURLListImporter
-                onDidImport={this.props.onClose}
-                addDirectories={this.props.addDirectories} />,
-
-              <GooninatorImporter
-                onDidImport={this.props.onClose}
-                addDirectories={this.props.addDirectories}
-                onChangeTextKind={this.props.onChangeTextKind}
-                onChangeTextSource={this.props.onChangeTextSource} />,
-            ]}
-          />
+        <GooninatorImporter
+            onDidImport={this.props.onClose}
+            addDirectories={this.props.addDirectories}
+            onChangeTextKind={this.props.onChangeTextKind}
+            onChangeTextSource={this.props.onChangeTextSource} />
       </Modal>
     );
   }
