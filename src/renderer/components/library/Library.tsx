b30174b305ed358c954ef3154f1b0c3af41c40d8
GoddessServant0 <goddessservant0@gmail.com>
2019-01-30 20:17:17 -0500

    Add Tag Manager

--- src/renderer/components/library/Library.tsx
+++ src/renderer/components/library/Library.tsx
@@ -1,21 +1,25 @@
 import * as React from 'react';
-import LibrarySource from "./LibrarySource";
 import {remote} from "electron";
+
+import LibrarySource from "./LibrarySource";
+import Tag from "./Tag";
 import Modal from "../ui/Modal";
 import URLModal from "../sceneDetail/URLModal";
 
 export default class Library extends React.Component {
   readonly props: {
     library: Array<LibrarySource>,
+    tags: Array<Tag>,
     onUpdateLibrary(sources: Array<LibrarySource>): void,
     onPlay(source: string): void,
     goBack(): void,
+    manageTags(): void,
   };
 
   readonly state = {
     removeAllIsOpen: false,
     urlImportIsOpen: false,
-    isEditing: String(null),
+    isEditing: -1,
   };
 
   render() {
@@ -26,6 +30,13 @@ export default class Library extends React.Component {
               <h2 className="Library__LibraryHeader">Library</h2>
           </div>
 
+          <div className="u-button-row-right">
+            <div
+                className="Library__ManageTags u-button u-clickable"
+                onClick={this.props.manageTags.bind(this)}>
+              Manage Tags
+            </div>
+          </div>
           <div className="BackButton u-button u-clickable" onClick={this.props.goBack}>Back</div>
         </div>
 
@@ -44,27 +55,27 @@ export default class Library extends React.Component {
           )}
           {this.props.library.map((source) =>
             <div className="Library__Source"
-                 key={(source.url as any) as number}>
-              {this.state.isEditing != source.url && (
+                 key={source.id}>
+              {this.state.isEditing != source.id && (
                 <div className="Library__SourceTitle u-clickable" onClick={this.props.onPlay.bind(this, source.url)}>
                   {source.url}
                 </div>
               )}
-              {this.state.isEditing == source.url && (
-                <form className="Library__SourceTitle" onSubmit={this.onEdit.bind(this, null)}>
+              {this.state.isEditing == source.id && (
+                <form className="Library__SourceTitle" onSubmit={this.onEdit.bind(this, -1)}>
                   <input
                       autoFocus
                       type="text"
                       value={source.url}
-                      onBlur={this.onEdit.bind(this, null)}
-                      onChange={this.onEditSource.bind(this, source.url)} />
+                      onBlur={this.onEdit.bind(this, -1)}
+                      onChange={this.onEditSource.bind(this, source.id)} />
                 </form>
               )}
 
               <div className="u-button u-destructive u-clickable"
-                 onClick={this.onRemove.bind(this, source.url)}>×️</div>
+                 onClick={this.onRemove.bind(this, source.id)}>×️</div>
               <div className="u-button u-edit u-clickable"
-                 onClick={this.onEdit.bind(this, source.url)}/>
+                 onClick={this.onEdit.bind(this, source.id)}/>
             </div>
           )}
         </div>
@@ -113,34 +124,35 @@ export default class Library extends React.Component {
     }
   }
 
-  onEdit(source: string, e: Event) {
+  onEdit(sourceID: number, e: Event) {
     e.preventDefault();
     // If user left input blank, remove it from list of sources
+    // Also prevent user from inputing duplicate source
     let newLibrary = [];
+    let newSources = Array<string>();
     let needsUpdate = false;
     for (let source of this.props.library) {
-      if (source.url == "") {
+      if (source.url == "" || newSources.includes(source.url)) {
         needsUpdate = true;
       } else {
         newLibrary.push(source);
+        newSources.push(source.url);
       }
     }
-    this.setState({isEditing: source});
+    this.setState({isEditing: sourceID});
     if (needsUpdate) { // Only update sources if we need to
       this.props.onUpdateLibrary(newLibrary);
     }
   }
 
-  onEditSource(oldURL: string, e: React.FormEvent<HTMLInputElement>) {
-    // Use the oldURL to make sure we edit the right source
+  onEditSource(sourceID: number, e: React.FormEvent<HTMLInputElement>) {
     let newLibrary = this.props.library;
     for (let source of newLibrary) {
-      if (source.url == oldURL) {
+      if (source.id == sourceID) {
         source.url = e.currentTarget.value;
         break;
       }
     }
-    this.setState({isEditing: e.currentTarget.value});
     this.props.onUpdateLibrary(newLibrary);
   }
 
@@ -149,8 +161,8 @@ export default class Library extends React.Component {
     this.props.onUpdateLibrary([]);
   }
 
-  onRemove(sourceURL: string) {
-    this.props.onUpdateLibrary(this.props.library.filter((s) => s.url != sourceURL));
+  onRemove(sourceID: number) {
+    this.props.onUpdateLibrary(this.props.library.filter((s) => s.id != sourceID));
   }
 
   onAdd() {
@@ -160,7 +172,7 @@ export default class Library extends React.Component {
   }
 
   onAddURL() {
-    this.setState({isEditing: ""});
+    this.setState({isEditing: this.props.library.length});
     this.addDirectories([""]);
   }
 
@@ -172,6 +184,7 @@ export default class Library extends React.Component {
     for (let url of directories) {
       let source = new LibrarySource();
       source.url = url;
+      source.id = newLibrary.length;
       newLibrary.push(source);
     }
     this.props.onUpdateLibrary(newLibrary);
