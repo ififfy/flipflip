cf3110db79d5dcd9d2407050eb28c48a4a37721e
Sissy Candice <goddessservant0@gmail.com>
2019-02-07 13:28:08 -0500

    Save window state

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,4 +1,5 @@
-import { app, BrowserWindow, session, Menu, MenuItem } from 'electron';
+import { app, BrowserWindow, session} from 'electron';
+import windowStateKeeper from 'electron-window-state';
 import * as path from 'path';
 import * as url from 'url';
 
@@ -14,11 +15,24 @@ function createWindow() {
         })
     });
 
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        height: 600,
-        width: 800,
+    // Load the previous state with fallback to defaults
+    let mainWindowState = windowStateKeeper({
+        defaultHeight: 800,
+        defaultWidth: 600,
     });
+
+    // Create the window using the state information
+    mainWindow = new BrowserWindow({
+        'x': mainWindowState.x,
+        'y': mainWindowState.y,
+        'width': mainWindowState.width,
+        'height': mainWindowState.height
+    });
+
+    // Let us register listeners on the window, so we can update the state
+    // automatically (the listeners will be removed when the window is closed)
+    // and restore the maximized or full screen state
+    mainWindowState.manage(mainWindow);
 
     // and load the index.html of the app.
     mainWindow.loadURL(
