9c4ff66359cef19acb91e38d382cdde08998bfdb
Sissy Candice <goddessservant0@gmail.com>
2019-02-19 12:55:22 -0500

    Quit FlipFlip when all windows close (#122)

--- src/main/main.ts
+++ src/main/main.ts
@@ -62,5 +62,10 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
