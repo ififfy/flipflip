c01ab233a68a4560429fd64f295b1ed597425853
StarrHelixx <StarrHelixx@gmail.com>
2019-08-14 18:42:39 -0400

    Skip OnlyFans posts

--- src/renderer/components/player/HeadlessScenePlayer.tsx
+++ src/renderer/components/player/HeadlessScenePlayer.tsx
@@ -670,8 +670,8 @@ function loadTwitter(config: Config, source: LibrarySource, filter: string, help
         let images = Array<string>();
         let lastID = "";
         for (let t of tweets) {
-          // Skip FanCentro posts
-          if (t.source.includes("https://fancentro.com")) continue;
+          // Skip FanCentro/OnlyFans posts
+          if (/href="https?:\/\/(fancentro|onlyfans)\.com\/?"/.exec(t.source) != null) continue;
           if (t.extended_entities && t.extended_entities.media) {
             for (let m of t.extended_entities.media) {
               if (m.video_info) {
