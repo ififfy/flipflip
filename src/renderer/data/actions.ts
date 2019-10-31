64b22808de7d93187e49af6883d534af68ad9d49
StarrHelixx <StarrHelixx@gmail.com>
2019-10-30 22:21:47 -0400

    More bugfixes

--- src/renderer/data/actions.ts
+++ src/renderer/data/actions.ts
@@ -35,13 +35,6 @@ import SceneGrid from "./SceneGrid";
 
 type State = typeof defaultInitialState;
 
-// TODO - Long Term
-//      Fix/Improve resonsive design
-//      Make audios ordered by default, allow synchronous playback
-//      Add UI option for excluding retweets (Fix markOffline)
-//      Resize ImageView when screen resizes ?
-//      Mosaic view for cards
-
 /** Getters **/
 
 // Returns true if the last route matches the given kind
@@ -1893,6 +1886,7 @@ export function importInstagram(getState: () => State, setState: Function) {
         const followingFeed = ig.feed.accountFollowing(id);
         followingFeed.deserialize(feedSession);
         if (!followingFeed.isMoreAvailable()) {
+          ig = null;
           setState({systemSnack: "Instagram Following Import has completed", progressMode: null, progressNext: null, progressCurrent: 0});
           return;
         }
