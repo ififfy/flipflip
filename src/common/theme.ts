851fd13b315f7e2f2f21d5dceacb2161275160c2
regtemp8 <regtemp8@proton.me>
2026-04-05 19:12:24 +0300

    Move shared data classes to common

--- src/common/theme.ts
+++ src/common/theme.ts
@@ -1,4 +1,38 @@
-const defaultTheme = {
+export interface Theme {
+  palette: {
+    mode: string,
+    primary: {
+      50: string,
+      100: string,
+      200: string,
+      300: string,
+      400: string,
+      500: string,
+      600: string,
+      700: string,
+      800: string,
+      900: string,
+      A100: string,
+      A200: string,
+      A400: string,
+      A700: string,
+      main: string,
+    },
+    secondary: {
+      main: string,
+    },
+    error: {
+      main: string,
+    },
+    text: {
+      hint: string,
+    },
+    // migration only
+    type?: string
+  }
+}
+
+const defaultTheme: Theme = {
   palette: {
     mode: "light",
     primary: {
